639aeafa2a2eae0ebef29d447a01fe90b8b788a1
Gourav Kumar Singh <gksingh@bootminds.com>
2019-06-14 20:58:44 +0530

    Create Bucket list

---
+++ djangorest/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import CreateView
+
+urlpatterns = [
+    path('bucketlists/', CreateView.as_view(), name="create"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
